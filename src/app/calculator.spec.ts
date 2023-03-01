47ad3bce2d9ec075fe1fe26fb2769c056a20833b
Juan Sáez García <juambersaezgarcia@gmail.com>
2023-03-01 18:34:00 +0100

    test: calculator tests

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,12 +1,30 @@
 import { Calculator } from './calculator';
 
 describe('Test for Calculator', () => {
-  it('#multiply should return a nine', () => {
+  it('#multiply should return 9', () => {
     //Arrange
     const calculator = new Calculator();
     //Act
-    const rta = calculator.multiply(3,3);
+    const rta = calculator.multiply(3, 3);
     //Assert
     expect(rta).toEqual(9);
+  });
+  it('#multiply should return 4', () => {
+    //Arrange
+    const calculator = new Calculator();
+    //Act
+    const rta = calculator.multiply(1, 4);
+    //Assert
+    expect(rta).toEqual(4);
+  });
+  it('#divide should return 2', () => {
+    //Arrange
+    const calculator = new Calculator();
+    //Act
+    const rta1 = calculator.divide(4, 2);
+    const rta2 = calculator.divide(6, 3);
+    //Assert
+    expect(rta1).toEqual(2);
+    expect(rta2).toEqual(2);
   });
 });
