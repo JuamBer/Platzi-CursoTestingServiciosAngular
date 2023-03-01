d88259ad2e362e2d07e2c23e5ba63d2a462d9086
Juan Sáez García <juambersaezgarcia@gmail.com>
2023-03-01 18:26:53 +0100

    feat: initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Calculator } from './calculator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'ng-testing-services';
+
+  ngOnInit(): void {
+    const calculator = new Calculator();
+    const rta = calculator.multiply(3, 3);
+    console.log(rta);
+    const rta2 = calculator.multiply(3, 0);
+    console.log(rta2);
+  }
+}
